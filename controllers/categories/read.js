df26645a1c0ea546d5b3dcf9148e740fb52d53d2
Nelson Aghemo <nelsonfaghemo@gmail.com>
2023-04-24 12:00:46 -0300

    b-09

--- controllers/categories/read.js
+++ controllers/categories/read.js
@@ -1,7 +1,21 @@
-let read =(req, res, next) =>
-  res.status(200).render("index", {
-    title: "/categories",
-    subtitle: "endpoints of categories",
-  });
+import Category from "./../../models/Category.js";
 
-  export default read
+let read = async (req, res, next) => {
+  //La funcion controladora debe ser asicrona para poder esperar la respuesta de mongo
+  try {
+    // utilizo la sintaccis de try catch para intetnar algo y catcehar los errores que puedan surgir
+    let all = await Category.find(); //Utilizo el metodo find para encontrar todos los datos del modelo (Que en este caso es category)
+    return res
+      .status(200) // configuro la respuesta que le tengo que enviar al cliente
+      .json({
+        categories: all,
+      });
+  } catch (err) {
+    console.log(error);
+    return res.status(400).json({
+      error: "¡Ha ocurrido un error!",
+    });
+  }
+};
+
+export default read;
